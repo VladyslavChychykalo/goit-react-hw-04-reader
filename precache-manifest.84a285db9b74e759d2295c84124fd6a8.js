a906260ccd248f2e430dd044499dd57861076d20
Vladyslav Chychykalo <vladyslav.chychykalo@gmail.com>
2019-12-08 20:44:06 +0200

    Updates

---
+++ precache-manifest.84a285db9b74e759d2295c84124fd6a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1533a152f0ef4dcc94ff45f35ad2378b",
+    "url": "/goit-react-hw-04-reader/index.html"
+  },
+  {
+    "revision": "baa1e9493b9dfa343182",
+    "url": "/goit-react-hw-04-reader/static/css/3.43ab3d36.chunk.css"
+  },
+  {
+    "revision": "558dd8dde3ad66d8017e",
+    "url": "/goit-react-hw-04-reader/static/css/main.d19f178a.chunk.css"
+  },
+  {
+    "revision": "c7fb2d19f2d019d33355",
+    "url": "/goit-react-hw-04-reader/static/js/2.5f668198.chunk.js"
+  },
+  {
+    "revision": "baa1e9493b9dfa343182",
+    "url": "/goit-react-hw-04-reader/static/js/3.853ae5bd.chunk.js"
+  },
+  {
+    "revision": "558dd8dde3ad66d8017e",
+    "url": "/goit-react-hw-04-reader/static/js/main.7611f64d.chunk.js"
+  },
+  {
+    "revision": "fe9fcb7eb9b6f4818aa4",
+    "url": "/goit-react-hw-04-reader/static/js/runtime-main.4b376a6b.js"
+  }
+]);
